34ea693ad2bbd61e2570286a66b755ada2fb63a6
Nolan Fuchs <nolanfuchs@nolans-mbp.domain>
2019-01-22 20:43:47 -0700

    Initial Commit

--- CADisplayLink/ViewController.swift
+++ CADisplayLink/ViewController.swift
@@ -9,12 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let countingLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Seconds"
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 18)
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.addSubview(countingLabel)
+        countingLabel.frame = view.frame
+        
+       // Create CA Dsiplay Link
+        let displayLink = CADisplayLink(target: self, selector: #selector(update))
+        displayLink.add(to: .main, forMode: .default)
     }
+    
+    var startValue: Double = 0
+    let endValue: Double = 120000
+    let animationDuration = 1.5
+    
+    let animationStartDate = Date()
 
+    @objc func update() {
+       let now = Date()
+        let elapsedTime = now.timeIntervalSince(animationStartDate)
+       
+        if elapsedTime > animationDuration {
+            self.countingLabel.text = "\(endValue)"
+        } else {
+        let percentage = elapsedTime / Double(animationDuration)
+        let value = startValue + percentage * (endValue - startValue)
+        self.countingLabel.text = "\(value)"
+        }
+    }
 
 }
 

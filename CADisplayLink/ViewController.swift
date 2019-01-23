d749a915ff685d11f05fd468621ebd6faf6717e3
Nolan Fuchs <nolanfuchs@nolans-mbp.domain>
2019-01-22 19:57:46 -0700

    Initial Commit

---
+++ CADisplayLink/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CADisplayLink
+//
+//  Created by Nolan Fuchs on 1/22/19.
+//  Copyright © 2019 Nolan Fuchs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
